4929157e6d9624b60c0cf25af7946498b2949d62
RufusOludare <toludare68@gmail.com>
2024-06-17 14:51:22 +0100

    experimenting with no breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss";
 
+
+const plugin = require('tailwind-utopia');
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +16,40 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      fontSize: {
+        'heading': 'clamp(1.5rem, 0.8831rem + 4.9351vi, 6.25rem)',
+        'paragraph': 'clamp(0.875rem, 0.8036rem + 0.3571vi, 1.125rem)'
+      },
+      spacing: {
+        'padding': "clamp(1rem, 0.7143rem + 1.4286vi, 2rem)",
+        'margin': ""
+      },
+      gridTemplateColumns: {
+        'responsive': 'repeat(auto-fit, minmax(clamp(300px, 10vw, 600px), 1fr))',
+      },
+      // gap: {
+      //   'fluid': 'clamp(1rem, 2vw, 2rem)',
+      // },
+
+      utopia: (theme: any) => ({
+        minScreen: theme('screens.md'),
+        minSize: 21,
+        minScale: 1,
+        maxScreen: '1440px',
+        maxSize: 100,
+        maxScale: 1,
+        textSizes: [],
+        spacingSizes: {},
+        spacingPairs: {},
+        spacingCustomPairs: [],
+      })
+
+
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-utopia')
+  ],
 };
 export default config;
